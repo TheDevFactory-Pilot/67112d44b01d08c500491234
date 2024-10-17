8fe43256fee4d839b8a33ad10d94bb2cd8e838c1
fsfdfds <herimion>
2024-10-17 16:24:15 +0000

    working app

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,52 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [goals, setGoals] = useState<string[]>([])
+  const [currentExp, setCurrentExp]=useState<string>('')
+
+  const onSubmitExpense=()=>{
+    if(!currentExp)return
+    setGoals((goals)=>[...goals,currentExp])
+    setCurrentExp('')
+  }
+
+  const deleteGoal=(id:number)=>{
+    //assuming indexes are the same
+    setGoals((currentGoals)=>currentGoals.filter((goal,index)=>index!=id))
+    
+  }
+
+
 
   return (
     <div  data-testId="MainApp">
       {/*Don't remove the data-testId as it's required for the system to detect that the app is live */}
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <h1>Add new goal</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <input 
+        data-testId='GoalInput'
+        onChange={(event)=>setCurrentExp(event.target.value)} 
+        value={currentExp}/>
+      <div className="card">
+        <button 
+        data-testId='Add Goal'
+        onClick={onSubmitExpense}>
+          Submit Expensve
+        </button>
+      </div>
+      <div>
+        {goals.map((goal,goalId)=>{
+          return(
+            <button data-testId={goal} onClick={()=>deleteGoal(goalId)}>
+              {goal}
+            </button>
+          )
+        })}
+      </div>
     </div>
   )
 }
